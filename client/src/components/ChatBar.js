37ff2b061c8c4f962d899c3f89ed9136aec81a0e
Bani Sholih <bani.sholih@icdexchange.com>
2023-05-11 17:09:31 +0700

    build chatapp using socket.io on react and nodejs

---
+++ client/src/components/ChatBar.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+const ChatBar = ({ socket }) => {
+  const [users, setUsers] = useState([]);
+
+  const namauser = localStorage.getItem('userName');
+
+  useEffect(() => {
+    socket.on('newUserResponse', (data) => setUsers(data));
+  }, [socket, users]);
+
+  return (
+    <div className="chat__sidebar">
+      <h2>Open Chat</h2>
+      <div>
+        <h4 className="chat__header">ACTIVE USERS - {namauser}</h4>
+        <div className="chat__users">
+          {users.map((user) => (
+            <p key={user.socketID}>{user.userName}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBar;
---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+//New imports
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+
+let users = [];
+
+//Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on('message', (data) => {
+    socketIO.emit('messageResponse', data);
+  });
+
+  socket.on('newUser', (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+  });
+
+  socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
+  socket.on('disconnect', () => {
+    users.splice(users.findIndex(elem => elem.socketID === socket.id), 1);
+    console.log(`${users.userName}: A user disconnected`);
+  });
+});
+
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
